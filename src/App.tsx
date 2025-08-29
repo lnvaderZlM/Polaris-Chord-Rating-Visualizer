339fb813dc69e39db4be4589b70bbdf370a795b0
Micah Alconcel <micah.alconcel@gmail.com>
2025-08-28 22:41:38 -0700

    styling, adding img visualization

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState, useRef } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import allChartConstants from '../src/chartConstants.json'
 import './index.css'
@@ -13,10 +11,16 @@ const difficulties = [
     "INFLUENCE"
 ];
 
+type ScoreData = {
+  paSkill: number;
+  id: string;
+  title: string;
+}
+
 function App() {
   const [scores, setScores] = useState([]);
   const [userRating, setUserRating] = useState(null);
-  const userScoresRef = useRef();
+  const userScoresRef = useRef(null);
 
   const calculatePaSkill = (base, cap, achievement, floor, ceil) => {
     const range = ceil - floor;
@@ -32,10 +36,11 @@ function App() {
 
     const musicData = userScores.data.score_data.usr_music_highscore.music;
 
-    const ratings = {};
+    const fullScoreData:ScoreData = {};
 
     for(let song of musicData) {
       const songTitle = song.name;
+      const songId = song.music_id;
       let playedCharts = song.chart_list.chart;
       if(!Array.isArray(playedCharts)) {
         playedCharts = [playedCharts];
@@ -45,8 +50,6 @@ function App() {
         const {achievement_rate, chart_difficulty_type} = playedChart;
         const chartConstant = Number(chartConstants[chart_difficulty_type]);
         if(achievement_rate < 8500) continue; //i arent dealing with that
-
-        const key = songTitle + " " + difficulties[chart_difficulty_type];
         let paSkill = chartConstant + 2.3;
         if(achievement_rate < 8500) {
           const base = chartConstant - 1;
@@ -69,28 +72,29 @@ function App() {
           const cap = chartConstant + 2.3;
           paSkill = calculatePaSkill(base, cap, achievement_rate, 9950, 10000);
         }
-        ratings[key] = paSkill;
+        fullScoreData[songTitle + " " + difficulties[chart_difficulty_type]] = {paSkill, id: songId, title: songTitle, difficulty: difficulties[chart_difficulty_type]}
       }
     }
 
-    const songs = Object.keys(ratings);
+    const songs = Object.keys(fullScoreData);
     songs.sort((a,b) => {
-      return ratings[a] < ratings[b] ? 1 : -1;
+      return fullScoreData[a].paSkill < fullScoreData[b].paSkill ? 1 : -1;
     })
-    const top30 = songs.slice(0, 30);
+    const top30Keys = songs.slice(0, 30);
 
     const top30Chart = [];
     let totalRating = 0;
-    for(let i = 0; i < top30.length; i++) {
-      top30Chart.push({
-        song: top30[i],
-        rating: ratings[top30[i]]
-      });
-      totalRating += ratings[top30[i]];
+    for(let i = 0; i < top30Keys.length; i++) {
+      const currScoreData = fullScoreData[top30Keys[i]];
+      top30Chart.push(currScoreData);
+      totalRating += currScoreData.paSkill;
     }
 
     setScores(top30Chart);
-    setUserRating(totalRating / 30);
+
+
+    const fixedRating = Math.trunc((totalRating / 30) * 100) / 100
+    setUserRating(fixedRating);
   };
 
   return (
@@ -101,7 +105,7 @@ function App() {
         </h1>
         <div className="flex flex-col items-start mb-4">
           <label for="scores-input">Input scores here:</label>
-          <textarea ref={userScoresRef} id="scores-input" className="bg-foreground w-full" rows="5"></textarea>
+          <textarea ref={userScoresRef} id="scores-input" className="bg-foreground text-background p-4 w-full" rows="5"></textarea>
         </div>
         <button onClick={calculateScores}>Calculate</button>
       </div>
@@ -115,16 +119,40 @@ function App() {
               <th>Rating</th>
             </tr>
             { scores.map((score, index) => {
-              console.log(score);
-              const {song, rating} = score;
+              const {paSkill, title, difficulty, songId} = score;
               return (
-                <tr key={index}>
-                  <td>{song}</td>
-                  <td>{rating.toFixed(2)}</td>
+                <tr key={index} className={`difficulty-${difficulties.indexOf(difficulty) + 1} border-b-1 border-background`}>
+                  <td className="px-4 py-2">{title} ({difficulty})</td>
+                  <td className="px-4 py-2">{paSkill.toFixed(2)}</td>
                 </tr>
               )
             })}
           </table>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+            { scores.map((score, index) => {
+              const {paSkill, title, difficulty, id} = score;
+              const currImage = `https://p.eagate.573.jp/game/polarischord/pc/img/music/jacket.html?c=${id}`;
+              return (
+                <div key={index} className={`difficulty-${difficulties.indexOf(difficulty) + 1} border-2 flex`}>
+                  <div className="w-1/3 relative">
+                    <img
+                      src={currImage}
+                      className="h-full object-cover"
+                      alt=""
+                    />
+                    {/* Gradient overlay */}
+                    <div className="absolute top-0 right-0 bottom-0 w-full bg-gradient-to-r pointer-events-none"></div>
+                  </div>
+
+                  {/* Text div */}
+                  <div className="w-2/3 flex flex-col justify-center">
+                    <div className="px-2">{title} ({difficulty})</div>
+                    <div className="px-2">{paSkill.toFixed(2)}</div>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
         </div>
       }
       
