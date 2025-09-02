ae7217777e41964d29efa9d15724c862738c1d73
Micah Alconcel <micah.alconcel@gmail.com>
2025-09-02 14:55:15 -0700

    updating vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Polaris-Chord-Rating-Visualizer/',
   plugins: [
     react(),
     tailwindcss(),
